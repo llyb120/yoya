c74987cf01aa2a343654e7a7816a9955fbe63d61
bin you <bin@bin>
2025-03-09 01:21:07 +0800

    增加strx包

--- syncx/holder.go
+++ syncx/holder.go
@@ -1,40 +1,47 @@
 package syncx
 
-import "sync"
+import (
+	"sync"
 
-type Holder[K comparable, V any] struct {
+	"github.com/petermattis/goid"
+)
+
+type Holder[V any] struct {
 	mu    sync.RWMutex
-	mp    map[K]V
+	mp    map[int64]V
 	newFn func() V
 }
 
-func NewHolder[K comparable, V any](fn func() V) *Holder[K, V] {
-	return &Holder[K, V]{newFn: fn, mp: make(map[K]V)}
+func NewHolder[V any](fn func() V) *Holder[V] {
+	return &Holder[V]{newFn: fn, mp: make(map[int64]V)}
 }
 
-func (h *Holder[K, V]) Get(key K) V {
+func (h *Holder[V]) Get() V {
 	if h.newFn == nil {
 		var zero V
 		return zero
 	}
 	h.mu.RLock()
 	defer h.mu.RUnlock()
-	if item, ok := h.mp[key]; ok {
+	goid := goid.Get()
+	if item, ok := h.mp[goid]; ok {
 		return item
 	}
 	item := h.newFn()
-	h.mp[key] = item
+	h.mp[goid] = item
 	return item
 }
 
-func (h *Holder[K, V]) Set(key K, value V) {
+func (h *Holder[V]) Set(value V) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	h.mp[key] = value
+	goid := goid.Get()
+	h.mp[goid] = value
 }
 
-func (h *Holder[K, V]) Del(key K) {
+func (h *Holder[V]) Del() {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	delete(h.mp, key)
+	goid := goid.Get()
+	delete(h.mp, goid)
 }
